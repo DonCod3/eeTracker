e0b9a1e1544864b67efc50b2b7b2f86113428200
Shamar MELVIN <shamar@MacBook-Pro.local>
2023-09-23 16:20:01 -0400

    routing + connectin

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// let Sequelize = require('sequelize');
+
+// //  protocol:// username:password@localhost:port/database
+// // const URI = `mysql2://root:Super98!@localhost:3306/bootcamp`
+// const URI = process.env.MYSQLURI
+
+// const sequelize = new Sequelize(URI);
+// module.exports = sequelize;
+
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "Super98!",
+        database: "eeTracker"
+    },
+    console.log("connected to the eTracker database")
+);
+
+module.exports = db
---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+const inquirer = require("inquirer");
+const { getDepartment, addDepartment, deleteDepartment } = require("./routes/department");
+const { getEmployee, addEmployee, deleteEmployee } = require("./routes/employee");
+const { getRoles, addRole, deleteRole } = require("./routes/role");
+
+const express = require("express");
+// const sequelize = require('./config/connection');
+const db = require("./config/connection");
+
+//Import and require mySQL2
+const mysql = require("mysql2");
+
+const PORT = process.env.PORT || 3001;
+const app = express()
+
+//middleware
+app.use(express.urlencoded({ extended: false }));
+
+//connect to database
+
+// sequelize.sync({ force: false }).then(() => {
+//     app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+// });
+
+
+db.connect (err => {
+    if(err) throw err;
+});
+
+
+  
